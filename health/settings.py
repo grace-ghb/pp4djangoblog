34857b806a399832a720b5c14592dc7f9c314f6a
Grace How <gracehow8702@gmail.com>
2024-01-09 18:00:18 +0000

    modified Procfile to 'web: gunicorn health.wsgi.aplication

--- health/settings.py
+++ health/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-graceghb-pp4djangoblog-lykuf17li53.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['pp4djangoblog-fe76bee41e28.herokuapp.com', '8000-graceghb-pp4djangoblog-lykuf17li53.ws-eu107.gitpod.io', 'localhost']
 
 
 # Application definition
