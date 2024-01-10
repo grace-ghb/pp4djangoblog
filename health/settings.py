e86a20012bddfa17eb6381979182b140818a10f9
Grace How <gracehow8702@gmail.com>
2024-01-10 13:08:46 +0000

    Heroku deploment

--- health/settings.py
+++ health/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['pp4djangoblog-fe76bee41e28.herokuapp.com', '8000-graceghb-pp4djangoblog-lykuf17li53.ws-eu107.gitpod.io', 'localhost']
 
